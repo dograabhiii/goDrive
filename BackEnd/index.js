4c79c807ad9b59b2192b88f556033309175aa7f6
dograabhiii <abhishekdogra2401@gmail.com>
2026-04-29 12:28:33 +0530

    fixed port for deployment

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -36,6 +36,9 @@ app.get("/prifill/:id",async(req,res)=>{
 })
 
 
-app.listen("7000",()=>{
-    console.log("server connected")
-})
+// process.env.PORT Render se port uthayega, aur agar nahi mila toh 7000 use karega
+const PORT = process.env.PORT || 7000; 
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server connected on port ${PORT}`);
+});
